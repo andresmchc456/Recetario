45611f67b1cceaec372be0f44bd823bc8ef03d17
Andrés Meléndez <andresmchc456@gmail.com>
2025-11-24 11:53:11 -0500

    feat: se realizo el cerra seccion

--- js/main.js
+++ js/main.js
@@ -9,6 +9,19 @@ async function cargarModulo(url, contenedorId) {
     const html = await response.text();
     document.getElementById(contenedorId).innerHTML = html;
 
+    // -----------------------------------------------
+    // Asegurar que favicon se cargue desde módulos head
+    // -----------------------------------------------
+    if (url.includes("head.html")) {
+      if (!document.querySelector("link[rel='icon']")) {
+        const favicon = document.createElement("link");
+        favicon.rel = "icon";
+        favicon.type = "image/png";
+        favicon.href = "../img/frito.png";
+        document.head.appendChild(favicon);
+      }
+    }
+
   } catch (error) {
     console.error(error);
     document.getElementById(contenedorId).innerHTML =
@@ -62,6 +75,28 @@ document.addEventListener("DOMContentLoaded", () => {
     cargarModulo("../inc/searchBar.html", "searchContainer");
     cargarModulo("../vistas/recetasPopulares.html", "recetasPopulares");
     cargarModulo("../vistas/categoriaRecetas.html", "categoriaRecetas");
+  }
+
+  // -----------------------------------------------------
+  // Si estamos en miPerfil.html → cargar navbar y footer
+  // -----------------------------------------------------
+  if (window.location.pathname.includes("miPerfil.html")) {
+    cargarModulo("../inc/navbar.html", "barraNavegacion");
+    cargarModulo("../inc/footer.html", "footerContainer");
+
+    // -----------------------------------------------
+    // Evento: Cerrar Sesión
+    // -----------------------------------------------
+    document.addEventListener("click", function (e) {
+      if (e.target && e.target.id === "btnCerrarSesion") {
+
+        // Borrar sesión
+        localStorage.clear();
+
+        // Redirigir al login
+        window.location.href = "../index.html";
+      }
+    });
   }
 
 });
